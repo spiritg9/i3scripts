2a13d014724c5be87e7e915c027a712ba40dde37
erni <fekete.erni@gmail.com>
2018-10-07 12:19:56 +0200

    added new improved openvpn script

--- i3_nix_ovpn/openvpn.py
+++ i3_nix_ovpn/openvpn.py
@@ -3,11 +3,11 @@ import os
 import sys
 
 def startIt(instance):
-    startCommand = "doas openvpn --config /home/erni/.ovpn/%s.ovpn --writepid /home/erni/.ovpn/%s.pid > /home/erni/.ovpn/%s.log" % (instance, instance, instance)
-    os.popen(startCommand)
+    startCommand = "doas openvpn --config /home/erni/.ovpn/%s.ovpn --writepid /home/erni/.ovpn/%s.pid --daemon --log /home/erni/.ovpn/%s.log" % (instance, instance, instance)
+    os.system(startCommand)
 
 def killIt(pid):
-    os.popen("doas kill -9 %s" % pid)
+    os.system("doas kill -9 %s" % pid)
 
 def printIt(text, color):
     print(text)
@@ -18,37 +18,38 @@ def isRunning(instance):
     filename = "/home/erni/.ovpn/%s.pid" % instance
     if os.path.isfile(filename):
         # get pid
-        with open(filename) as f:
-            pid = f.read().strip()
-            isRunning = os.popen("ps -p %s -o comm=" % pid)
-            isRunning = isRunning.read().strip()
+        pid = os.popen("doas cat %s" % filename)
+        pid = pid.read().strip()
 
-            if len(isRunning) > 0:
-                return pid
+        isRunning = os.popen("ps -p %s -o comm=" % pid)
+        isRunning = isRunning.read().strip()
+
+        if len(isRunning) > 0:
+            return pid
     return "" 
 
+if __name__ == "__main__":
+   # get instance
+    if not "BLOCK_INSTANCE" in os.environ:
+        exit(0)
+    instance = os.environ["BLOCK_INSTANCE"]
 
-# get instance
-if not "BLOCK_INSTANCE" in os.environ:
-    exit()
-instance = os.environ["BLOCK_INSTANCE"]
+    pid = isRunning(instance)
 
-pid = isRunning(instance)
-
-if pid != "":
-    color = "#00ff00"
-    text = "UP"
-else:
-    color = "#ff0000"
-    text = "DOWN"
+    if pid != "":
+        color = "#00ff00"
+        text = "UP"
+    else:
+        color = "#ff0000"
+        text = "DOWN"
 
 
-if "BLOCK_BUTTON" in os.environ:
-    buttonValue = os.environ["BLOCK_BUTTON"]
-    if buttonValue == "1" and pid == "":
-        startIt(instance)
-    elif buttonValue == "3" and pid != "":
-        killIt(pid)
+    if "BLOCK_BUTTON" in os.environ:
+        buttonValue = os.environ["BLOCK_BUTTON"]
+        if buttonValue == "1" and pid == "":
+            startIt(instance)
+        elif buttonValue == "3" and pid != "":
+            killIt(pid)
 
-printIt(text, color)
+    printIt(text, color)
 
